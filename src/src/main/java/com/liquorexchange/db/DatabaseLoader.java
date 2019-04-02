c6b60b78f848012b2c8b88c85b27bea7a62aa055
Karel Zavadil <mojemejly@centrum.cz>
2019-04-02 00:12:17 +0000

    conditional db reset

--- src/src/main/java/com/liquorexchange/db/DatabaseLoader.java
+++ src/src/main/java/com/liquorexchange/db/DatabaseLoader.java
@@ -1,13 +1,21 @@
 package com.liquorexchange.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import com.liquorexchange.db.repository.*;
 import com.liquorexchange.db.model.*;
 
+/**
+ * If com.liquorexchange.resetdb in application.properties is set to true,
+ * all data in database will be deleted and new fresh set of test data will be created
+ */
 @Component
 public class DatabaseLoader implements CommandLineRunner {
+
+	@Value("${com.liquorexchange.resetdb}")
+	private boolean reset_db;
 
 	private final CountryRepository country_repository;
 	private final AuctionRepository auction_repository;
@@ -20,12 +28,16 @@ public class DatabaseLoader implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		Country czech_rep = new Country("CZ", "Czech Republic");
-		this.country_repository.save(czech_rep);
-		this.country_repository.save(new Country("US", "United States"));
+		if (this.reset_db) {
+			this.country_repository.deleteAll();
+			this.auction_repository.deleteAll();
+			
+			Country czech_rep = new Country("CZ", "Czech Republic");
+			this.country_repository.save(czech_rep);
+			this.country_repository.save(new Country("US", "United States"));
 
-		this.auction_repository.save(new Auction("test auction 1", czech_rep));
-		this.auction_repository.save(new Auction("test auction 2", czech_rep));
-
+			this.auction_repository.save(new Auction("test auction 1", czech_rep));
+			this.auction_repository.save(new Auction("test auction 2", czech_rep));
+		}
 	}
 }
