c3fa2994d90ffde185c16b30256c749f66b5e737
Tomas Navratil <tomnavr@gmail.com>
2019-04-05 04:31:16 +0200

    Add login form

---
+++ src/src/main/js/pages/auth/Login.tsx
@@ -0,0 +1,43 @@
+import React, {FC} from 'react';
+import * as yup from 'yup';
+
+import Container from '../../components/Container';
+import Box from '../../components/Box';
+import Form from '../../components/Form';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const USER_SCHEMA = yup.object().shape({
+	name: yup.string().required('This is a required field'),
+	password: yup.string().required('This is a required field'),
+});
+
+interface IUser {
+	name: string;
+	password: string;
+}
+
+const Login: FC = () => {
+	const handleSubmit = (user: IUser) => {
+		// tslint:disable-next-line: no-console
+		console.log('LOG IN', user);
+	};
+
+	return (
+		<Container>
+			<Box title="Log In">
+				<Form
+					initialValues={{name: '', password: ''}}
+					schema={USER_SCHEMA}
+					onSubmit={handleSubmit}
+				>
+					<Input id="name" label="Name" />
+					<Input id="password" label="Password" type="password" />
+					<Button type="submit" label="Submit" primary />
+				</Form>
+			</Box>
+		</Container>
+	);
+};
+
+export default Login;
