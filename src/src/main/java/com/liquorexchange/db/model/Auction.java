ad7ac65584a2f858029bea770755ffb800b01c8d
tetour <david.tetour@jumpshot.com>
2019-04-10 20:22:34 +0200

    Generated getters and setters for model objects.

--- src/src/main/java/com/liquorexchange/db/model/Auction.java
+++ src/src/main/java/com/liquorexchange/db/model/Auction.java
@@ -25,4 +25,28 @@ public class Auction {
 		this.name = name;
 		this.countryCode = country.getCode();
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
 }
--- src/src/main/java/com/liquorexchange/db/model/Country.java
+++ src/src/main/java/com/liquorexchange/db/model/Country.java
@@ -19,4 +19,20 @@ public class Country {
 		this.code = code;
 		this.name = name;
 	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
