2a64f8e257ce2824128315f9f9f6a1e889bf624d
Tomas Navratil <tomnavr@gmail.com>
2019-04-13 19:35:00 +0200

    Fix war build to run in tomcat

--- src/src/main/java/com/liquorexchange/LiquorExchangeApplication.java
+++ src/src/main/java/com/liquorexchange/LiquorExchangeApplication.java
@@ -2,10 +2,17 @@ package com.liquorexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LiquorExchangeApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(LiquorExchangeApplication.class, args);
-	}
+public class LiquorExchangeApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LiquorExchangeApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquorExchangeApplication.class, args);
+    }
 }
