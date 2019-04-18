a7b3779787415d31607d749bc53822c1259a2c9d
karel <karel@zavadil.eu>
2019-04-18 13:23:34 +0200

    - added executable bit to scripts - removed mongo user and host/port (using defaults) - application.properties now work with docker - added example config

--- src/docker/docker-entrypoint.sh
+++ src/docker/docker-entrypoint.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 mongod --fork --logpath /var/log/mongo.log
-mongo --verbose localhost:27017 <<EOF
-use liquor
-db.createUser(
-  {
-    user: "usr",
-    pwd: "xyz123",
-    roles: [ { role: "readWrite", db: "liquor" } ]
-  }
-)
-EOF
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=docker -jar /app.jar
