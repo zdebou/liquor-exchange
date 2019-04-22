4f96c988721769a11b30a1c9bcded8c91b813ea9
tetour <david.tetour@jumpshot.com>
2019-04-22 18:41:09 +0200

    Deleted unnecessary getters, setters that are generated from lombok.

--- src/src/main/java/com/liquorexchange/db/model/Auction.java
+++ src/src/main/java/com/liquorexchange/db/model/Auction.java
@@ -26,28 +26,4 @@ public class Auction {
 		this.name = name;
 		this.country = country;
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Country getCountry() {
-		return country;
-	}
-
-	public void setCountry(Country country) {
-		this.country = country;
-	}
 }
--- src/src/main/java/com/liquorexchange/db/model/Country.java
+++ src/src/main/java/com/liquorexchange/db/model/Country.java
@@ -19,20 +19,4 @@ public class Country {
 		this.code = code;
 		this.name = name;
 	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
