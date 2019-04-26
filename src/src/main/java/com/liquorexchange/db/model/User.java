042a4752ab8bafda6607d8a09eeddec92f814d01
janseliga <js>
2019-04-26 15:10:56 +0200

    Added security model classes

--- src/src/main/java/com/liquorexchange/db/model/User.java
+++ src/src/main/java/com/liquorexchange/db/model/User.java
@@ -7,12 +7,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Document
 public class User {
-
     @Id
     private String username;
 
@@ -25,6 +25,7 @@ public class User {
     @NotBlank
     private String lastName;
 
+    @NotBlank
     private String password;
 
     private String identityCardNumber;
@@ -41,4 +42,29 @@ public class User {
 
     private Integer VATNumber;
 
+    private List<Role> roles;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
--- src/src/main/java/com/liquorexchange/db/repository/UserRepository.java
+++ src/src/main/java/com/liquorexchange/db/repository/UserRepository.java
@@ -3,4 +3,13 @@ package com.liquorexchange.db.repository;
 import com.liquorexchange.db.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    List<User> findByUsernameIn(List<String> usernames);
+
+    Boolean existsByUsername(String username);
+}
