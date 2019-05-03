d06efe6cd742bc283a852cfe0f06a54749ed9e99
janseliga <js>
2019-05-03 11:51:26 +0200

    Added custom exceptions

--- src/src/main/java/com/liquorexchange/config/SecurityConfig.java
+++ src/src/main/java/com/liquorexchange/config/SecurityConfig.java
@@ -89,8 +89,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
 
-        // Add our custom JWT security filter
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
     }
 }
