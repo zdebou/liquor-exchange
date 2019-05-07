f37a56a7aa29d1b3ee44cc2791856bcf5372206e
David Štorek <storek.david@seznam.cz>
2019-05-08 00:04:14 +0200

    HTML cucumber reports added. Results can be found in target/cucumber-reports/report.html

--- src/src/test/java/runner/TestRunner.java
+++ src/src/test/java/runner/TestRunner.java
@@ -5,9 +5,11 @@
  */
 package runner;
 
+import com.cucumber.listener.Reporter;
 import common.TestUtils;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import java.io.File;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
@@ -19,11 +21,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features", 
-        glue = {"seleniumgluecode"}
+        glue = {"seleniumgluecode"},
+        plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
+        monochrome = true
 )
 public class TestRunner {
     @AfterClass
     public static void afterTests() {
         TestUtils.closeDriver();
+        Reporter.loadXMLConfig(new File("report.xml"));
     }
 }
